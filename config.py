47ed3ee11107dd2494a512d9481f45b3810e7133
rishabhp456 <patilrisshabh22@gmail.com>
2026-07-03 23:44:07 +0530

    update db url

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 import os
 
 FLASK_HOST = '0.0.0.0'
-FLASK_PORT = "5000"
-
-MONGO_URL = "mongodb://localhost:27017"
+FLASK_PORT = "8000"
+MONGO_USERNAME = "rishabhp"
+MONGO_PASSWORD = "test123$"
+MONGO_URL = f"mongodb+srv://{MONGO_USERNAME}:{MONGO_PASSWORD}@docdb-cluster-20260630-0923.global.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000"
 db_name = 'Teen_Depression_db'
 user_collection_name = 'user_data'
 data_collection_name = 'model_data'
