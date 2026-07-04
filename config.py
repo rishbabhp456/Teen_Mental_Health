91b84e8cceb12f906ced9a773785f22761d95854
rishabhp456 <patilrisshabh22@gmail.com>
2026-07-04 08:59:51 +0530

    added std_scalar path

--- config.py
+++ config.py
@@ -9,6 +9,7 @@ db_name = 'Teen_Depression_db'
 user_collection_name = 'user_data'
 data_collection_name = 'model_data'
 
+STD_SCALER_PATH = os.path.join(os.getcwd(),'artifacts','std_scaler.pkl')
 ML_MODEL_PATH = os.path.join(os.getcwd(),'artifacts','Teen_Depr_final_model.pkl')
 JSON_DATA_PATH = os.path.join(os.getcwd(),'artifacts','Teen_depr_coln_data.json')
 INPUT_DATA_PATH = os.path.join(os.getcwd(),'data',"Teen_Mental_Health_Dataset.csv")
